c3b3fb4c456e05a6b8995a04338596060ed6c3bc
George Zakhary <elprincedoca@gmail.com>
2026-07-27 22:39:09 +0300

    fix: Update STATIC_URL and STATIC_ROOT paths for consistency; enhance video iframe attributes for better performance and security

--- learning_platform/settings.py
+++ learning_platform/settings.py
@@ -169,11 +169,11 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static')
+   BASE_DIR / 'static'
 ]
-STATIC_ROOT = "collectstatic/"
+STATIC_ROOT = BASE_DIR / "collectstatic/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
